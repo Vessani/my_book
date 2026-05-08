79ce225d339dd85da0994ef29190cac82e4b7ecf
vessani <alanvessanidev@gmail.com>
2026-05-07 22:26:10 -0300

    connection db inicialized

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module my-books/web-service-gin
 
 go 1.26.2
 
-require github.com/gin-gonic/gin v1.12.0
+require (
+	github.com/gin-gonic/gin v1.12.0
+	github.com/lib/pq v1.12.3
+)
 
 require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
--- main.go
+++ main.go
@@ -1,29 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 
 type book struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Author string `json:"author"`
-	Price float64 `json:"price"`
+	ID int `json:"id"`
+	Title string `json:"title" binding:"required"`
+	Author string `json:"author" bindgin:"required"`
+	Price float64 `json:"price" binding:"required"`
 }
 
 
-func main() {
+/* func main() {
 	router := gin.Default()
 	router.GET("/books", getBooks)
 	router.POST("/books", postBooks)
 	router.Run("localhost:8080")
 }
+*/
+
+func main() {
+	connStr := "user=postgres password=example dbname=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	
+	err = db.Ping()
+
+	if err != nil {
+		fmt.Println("Erro não consegui me conectar ao db!")
+		panic(err)
+	}
+
+	fmt.Println("Sucesso, conectado ao db!")
+}
 
 var books = []book{
+
 	{ID: "1", Title: "The Fellowship of the Ring", Author: "J.J Tolkien", Price: 25.99},
 	{ID:"2", Title: "The Name of the Wind", Author: "Patrick Rothfuss", Price: 15.50},
 	{ID: "3", Title: "Animal Farm", Author: "George Orwell", Price: 13.00},
