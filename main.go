5a8e48cdc4efc5f1cea9ae2f4ec5406678efcd24
vessani <alanvessanidev@gmail.com>
2026-05-09 21:55:53 -0300

    create post book

--- main.go
+++ main.go
@@ -21,16 +21,16 @@ type BookHandler struct {
 }
 
 func main() {
-	// 1. Configuração da Conexão
+
 	connStr := "user=postgres password=example dbname=postgres sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		panic(err)
 	}
-	// O defer garante que o banco só feche quando o SERVIDOR parar
+
 	defer db.Close()
 
-	// 2. Verificação do Banco
+
 	err = db.Ping()
 	if err != nil {
 		fmt.Println("Erro: não consegui me conectar ao db!")
@@ -38,16 +38,16 @@ func main() {
 	}
 	fmt.Println("Sucesso, conectado ao db!")
 
-	// 3. Inicialização do Handler com a Injeção de Dependência
+
 	handler := &BookHandler{DB: db}
 
-	// 4. Configuração das Rotas
+
 	router := gin.Default()
 	
-	// Registro da rota POST
+
 	router.POST("/books", handler.postBooks)
 
-	// 5. Execução do Servidor (O programa "trava" aqui e fica esperando o curl)
+
 	fmt.Println("Servidor rodando em http://localhost:8080")
 	router.Run("localhost:8080")
 }
@@ -60,7 +60,7 @@ func (h *BookHandler) postBooks(c *gin.Context) {
 		return
 	}
 
-	// Dica: Certifique-se que o nome da tabela no DB é "books" (minúsculo é o padrão SQL)
+
 	query := `INSERT INTO books (title, author, price) VALUES ($1, $2, $3) RETURNING id`
 
 	err := h.DB.QueryRow(query, newBook.Title, newBook.Author, newBook.Price).Scan(&newBook.ID)
